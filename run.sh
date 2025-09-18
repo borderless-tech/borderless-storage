4a97f5b38d8c3b21d0a4bfdb5b89bba0fa039f71
Christian Hammermeister <christian.hammermeister@borderless-technologies.com>
2025-09-18 19:24:23 +0200

    uncomment timeout

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ cd $(dirname $0)
 export IP_ADDR="127.0.0.1:3000"
 export DATA_DIR="db"
 export DOMAIN="http://localhost:3000"
-export RQ_TIMEOUT_SECS=1
+#export RQ_TIMEOUT_SECS=1
 #export TTL_ORPHAN_SECS=30 # for testing
 
 cargo run
